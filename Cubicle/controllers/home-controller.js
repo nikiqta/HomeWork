15bbc121bb6185027881af325fb2e4ed349d183a
Nedelcho Karageorgiev <nedelcho.karageorgiev@Softwaregroup-bg.com>
2019-01-29 14:28:15 +0200

    Cube HomeWork

--- Cubicle/controllers/home-controller.js
+++ Cubicle/controllers/home-controller.js
@@ -1,10 +1,14 @@
 const cubeApi = require('./../api/cubeApi.js');
+const errors = require('./../utilities/errorHandling.js');
 
 module.exports = {
 
     getHome: async (req, res) => {
         const { search, from, to } = req.query;
         const emptyDB = 'There are currently no cubes in the data base yet!';
+
+       const fromNumber = from === NaN ? 1 : from;
+       const toNumber = to === NaN  ? 6 : to;
 
         if(!search && !from && !to) {
             try {
@@ -16,6 +20,24 @@ module.exports = {
                 } catch (err) {
                     console.error(err.message);
                 }
+        } else if ((1 > fromNumber || fromNumber > 6) || (1 > toNumber || toNumber > 6)) {
+            const error = errors.error('input validation fieldError');
+            res.render('home/home', {
+               cubes: [],
+               fieldErr: error,
+               search,
+               from,
+               to
+            })
+        } else if (fromNumber > toNumber) {
+            const error = errors.error('input validation wrong order');
+            res.render('home/home', {
+               cubes: [],
+               fieldErr: error,
+               search,
+               from,
+               to
+            })
         } else {
             try {
                 const cubes = await cubeApi.getFilteredCubes(req.query);
--- Cubicle/utilities/errorHandling.js
+++ Cubicle/utilities/errorHandling.js
@@ -3,14 +3,20 @@ function error(err) {
     const nameErr = 'Name must be between 3 and 15 symbols!';
     const descErr = 'Description must be between 20 and 300 symbols!';
     const imageUrlErr = 'ImageURL must begin with "https://" and end with ".jpg" or ".png"';
+    const fieldErr = 'Both field  to and from must be in range of 1 - 6';
+    const wrongOrderErr = 'To value must be lower than or equal to From value';
 
 
     if( err.startsWith('Cube validation failed: name')) {
         return nameErr;
     } else if(err.startsWith('Cube validation failed: description')){
         return descErr;
-    } else if('Cube validation failed: imageURL') {
+    } else if(err === 'Cube validation failed: imageURL') {
         return imageUrlErr;
+    } else if (err === 'input validation fieldError') {
+        return fieldErr;
+    } else if (err === 'input validation wrong order') {
+        return wrongOrderErr;
     }
     return
 }
